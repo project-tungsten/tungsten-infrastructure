a76d8965bc45eb2ab31a0e20c4cabbb2d150a235
Fabio Maia <fabioromm@gmail.com>
2017-06-13 22:35:57 -0400

    Added aws terraform provisioner

---
+++ provisioning/aws/cluster.tf
@@ -0,0 +1,84 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+// CentOS Linux 7 x86_64 HVM EBS 1704_01
+variable "aws_ami" { default = "ami-d52f5bc3" }
+variable "ssh_public_key_path" {}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
+
+resource "aws_key_pair" "terraform" {
+  public_key = "${file("${var.ssh_public_key_path}")}"
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "tungsten-k8s-master-server-1" {
+  ami           = "${var.aws_ami}"
+  instance_type = "t2.medium"
+  key_name = "${aws_key_pair.terraform.id}"
+  security_groups = ["allow_all"]
+  tags {
+    "kargo-kube-master" = "true"
+  }
+}
+
+resource "aws_instance" "tungsten-etcd-server-1" {
+  ami           = "${var.aws_ami}"
+  instance_type = "t2.medium"
+  key_name = "${aws_key_pair.terraform.id}"
+  security_groups = ["allow_all"]
+  tags {
+    "kargo-etcd" = "true"
+  }
+}
+
+resource "aws_instance" "tungsten-etcd-server-2" {
+  ami           = "${var.aws_ami}"
+  instance_type = "t2.medium"
+  key_name = "${aws_key_pair.terraform.id}"
+  security_groups = ["allow_all"]
+  tags {
+    "kargo-etcd" = "true"
+  }
+}
+
+resource "aws_instance" "tungsten-k8s-worker-server-1" {
+  ami           = "${var.aws_ami}"
+  instance_type = "t2.large"
+  key_name = "${aws_key_pair.terraform.id}"
+  security_groups = ["allow_all"]
+  tags {
+    "kargo-kube-node" = "true"
+  }
+}
+
+resource "aws_instance" "tungsten-k8s-worker-server-2" {
+  ami           = "${var.aws_ami}"
+  instance_type = "t2.large"
+  key_name = "${aws_key_pair.terraform.id}"
+  security_groups = ["allow_all"]
+  tags {
+    "kargo-kube-node" = "true"
+  }
+}
