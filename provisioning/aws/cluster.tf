982a6b3aa86b3677a5ac15b02ce1f1ce4bd167a3
Fabio Maia <fabioromm@gmail.com>
2017-06-15 19:57:01 -0400

    Added route53 resource

--- provisioning/aws/cluster.tf
+++ provisioning/aws/cluster.tf
@@ -1,13 +1,15 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 // CentOS Linux 7 x86_64 HVM EBS 1704_01
-variable "aws_ami" { default = "ami-d52f5bc3" }
+variable "aws_ami" {
+  default = "ami-d52f5bc3"
+}
 variable "ssh_public_key_path" {}
 
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_key_pair" "terraform" {
@@ -15,69 +17,83 @@ resource "aws_key_pair" "terraform" {
 }
 
 resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
+  name = "allow_all"
   description = "Allow all inbound traffic"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
 }
 
 resource "aws_instance" "tungsten-k8s-master-server-1" {
-  ami           = "${var.aws_ami}"
+  ami = "${var.aws_ami}"
   instance_type = "t2.medium"
   key_name = "${aws_key_pair.terraform.id}"
-  security_groups = ["allow_all"]
+  security_groups = [
+    "allow_all"
+  ]
   tags {
     "kargo-kube-master" = "true"
   }
 }
 
 resource "aws_instance" "tungsten-etcd-server-1" {
-  ami           = "${var.aws_ami}"
+  ami = "${var.aws_ami}"
   instance_type = "t2.medium"
   key_name = "${aws_key_pair.terraform.id}"
-  security_groups = ["allow_all"]
+  security_groups = [
+    "allow_all"
+  ]
   tags {
     "kargo-etcd" = "true"
   }
 }
 
 resource "aws_instance" "tungsten-etcd-server-2" {
-  ami           = "${var.aws_ami}"
+  ami = "${var.aws_ami}"
   instance_type = "t2.medium"
   key_name = "${aws_key_pair.terraform.id}"
-  security_groups = ["allow_all"]
+  security_groups = [
+    "allow_all"
+  ]
   tags {
     "kargo-etcd" = "true"
   }
 }
 
 resource "aws_instance" "tungsten-k8s-worker-server-1" {
-  ami           = "${var.aws_ami}"
+  ami = "${var.aws_ami}"
   instance_type = "t2.large"
   key_name = "${aws_key_pair.terraform.id}"
-  security_groups = ["allow_all"]
+  security_groups = [
+    "allow_all"
+  ]
   tags {
     "kargo-kube-node" = "true"
   }
 }
 
 resource "aws_instance" "tungsten-k8s-worker-server-2" {
-  ami           = "${var.aws_ami}"
+  ami = "${var.aws_ami}"
   instance_type = "t2.large"
   key_name = "${aws_key_pair.terraform.id}"
-  security_groups = ["allow_all"]
+  security_groups = [
+    "allow_all"
+  ]
   tags {
     "kargo-kube-node" = "true"
   }
@@ -89,8 +105,21 @@ resource "aws_route53_zone" "primary" {
 
 resource "aws_route53_record" "k8s-api" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
-  name    = "k8s-api.project-tungsten.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.tungsten-k8s-master-server-1.public_ip}"]
+  name = "k8s-api.project-tungsten.com"
+  type = "A"
+  ttl = "300"
+  records = [
+    "${aws_instance.tungsten-k8s-master-server-1.public_ip}"
+  ]
+}
+
+resource "aws_route53_record" "k8s-routes" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "*.project-tungsten.com"
+  type = "A"
+  ttl = "300"
+  records = [
+    "${aws_instance.tungsten-k8s-worker-server-1.public_ip}",
+    "${aws_instance.tungsten-k8s-worker-server-2.public_ip}"
+  ]
 }
